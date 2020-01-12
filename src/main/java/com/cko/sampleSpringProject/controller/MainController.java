3b860d2d43992b9a9c540b6ee5409057ef6193f7
nikita1315 <you@example.com>
2020-01-12 10:58:57 +0300

    First commit

---
+++ src/main/java/com/cko/sampleSpringProject/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.cko.sampleSpringProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/loginPage")
+    public String showLoginPage(){
+
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String showMainPage(){
+        return "mainPage";
+    }
+
+    @GetMapping("/test")
+    public String showTestPage(){
+        return "test";
+    }
+
+
+    @GetMapping("/calculator")
+    public String showCalculatorPage(){
+        return "Calculator";
+    }
+
+}
